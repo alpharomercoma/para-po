29ae49012b9475eac5730397b5245a66c8c2d8cc
Alpha Romer Coma <alpharomercoma@proton.me>
2024-10-23 14:20:44 +0800

    refactor: initial landing screen

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,13 @@
-import NavBar from "@/components/Navbar";
-import Route from "@/components/Route";
+import LandingAnimation from "@/components/landing/landingAnimation";
+
 const Home = () => {
   return (
-    <main className="w-full">
-      <NavBar />
-      <Route />
-    </main>
+    <div className="flex justify-center items-center h-screen bg-gray-100">
+      <div className="relative flex justify-center items-center w-full h-full">
+        <LandingAnimation />
+      </div>
+    </div>
   );
-}
-
+};
 
 export default Home;
---
+++ src/components/landing/landingAnimation.tsx
@@ -0,0 +1,112 @@
+"use client";
+
+import { useEffect, useRef } from "react";
+import { gsap } from "gsap";
+
+const letters = [
+  {
+    letter: "P",
+    color: "blue",
+    rotate: 10,
+    size: "text-[12rem]",
+    left: 0,
+    top: 0,
+  },
+  {
+    letter: "a",
+    color: "pink",
+    rotate: 20,
+    size: "text-[10rem]",
+    left: 20,
+    top: 40,
+  },
+  {
+    letter: "r",
+    color: "green",
+    rotate: -15,
+    size: "text-[11rem]",
+    left: -10,
+    top: 80,
+  },
+  {
+    letter: "a",
+    color: "yellow",
+    rotate: 0,
+    size: "text-[10rem]",
+    left: 20,
+    top: 100,
+  },
+  {
+    letter: "p",
+    color: "pink",
+    rotate: -10,
+    size: "text-[12rem]",
+    left: -10,
+    top: 140,
+  },
+  {
+    letter: "o",
+    color: "green",
+    rotate: 10,
+    size: "text-[10rem]",
+    left: 20,
+    top: 200,
+  },
+];
+
+export default function Component() {
+  const letterRefs = useRef<(HTMLDivElement | null)[]>([]);
+
+  useEffect(() => {
+    letterRefs.current.forEach((el, index) => {
+      gsap.fromTo(
+        el,
+        {
+          y: 0,
+          x: letters[index].left,
+          opacity: 0,
+          rotation: letters[index].rotate,
+        },
+        {
+          y: letters[index].top,
+          x: letters[index].left,
+          opacity: 1,
+          duration: 1,
+          delay: index * 0.2,
+          ease: "bounce.out",
+        }
+      );
+    });
+  }, []);
+
+  return (
+    <div className="relative h-screen bg-white">
+      <div className="relative w-fit">
+        {letters.map((letterObj, index) => (
+          <div
+            key={index}
+            ref={(el) => {
+              letterRefs.current[index] = el;
+            }}
+            className={`absolute ${
+              letterObj.color === "blue"
+                ? "text-blue-500"
+                : letterObj.color === "pink"
+                ? "text-pink-500"
+                : letterObj.color === "green"
+                ? "text-green-500"
+                : "text-yellow-500"
+            } ${letterObj.size} font-bold`}
+            style={{
+              transform: `rotate(${letterObj.rotate}deg)`,
+              left: `${letterObj.left}px`,
+              top: `${letterObj.top}px`,
+            }}
+          >
+            {letterObj.letter}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
